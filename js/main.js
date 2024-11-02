9330deda097d22dc29c702b6822be8b5e173cc8c
Ahmed Hassan <76397258+xZegAs@users.noreply.github.com.>
2024-11-02 09:29:31 +0200

    initial commit
    	new file:   css/all.min.css
    	new file:   css/bootstrap.min.css
    	new file:   css/style.css
    	new file:   images/1.jpg
    	new file:   images/1_New1.jpg
    	new file:   images/2.jpg
    	new file:   images/3.jpg
    	new file:   images/4.jpg
    	new file:   images/5.jpg
    	new file:   images/6.jpg
    	new file:   images/about.jpg
    	new file:   images/bg.jpg
    	new file:   images/blog-1.jpg
    	new file:   images/blog-2.jpg
    	new file:   images/blog-3.jpg
    	new file:   images/dots.png
    	new file:   images/favicon.ico
    	new file:   images/header-bg.jpg
    	new file:   images/hero.jpg
    	new file:   images/logo-dark.png
    	new file:   images/logo.png
    	new file:   images/portfolio/1.jpg
    	new file:   images/portfolio/2.jpg
    	new file:   images/portfolio/3.jpg
    	new file:   images/portfolio/4.jpg
    	new file:   images/portfolio/5.jpg
    	new file:   images/portfolio/6.jpg
    	new file:   images/testi-1.jpg
    	new file:   images/testi-2.jpg
    	new file:   images/testi-3.jpg
    	new file:   index.html
    	new file:   js/bootstrap.bundle.min.js
    	new file:   js/main.js
    	new file:   webfonts/fa-brands-400.ttf
    	new file:   webfonts/fa-brands-400.woff2
    	new file:   webfonts/fa-regular-400.ttf
    	new file:   webfonts/fa-regular-400.woff2
    	new file:   webfonts/fa-solid-900.ttf
    	new file:   webfonts/fa-solid-900.woff2
    	new file:   webfonts/fa-v4compatibility.ttf
    	new file:   webfonts/fa-v4compatibility.woff2

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed("#element", {
+  strings: ["Larry Daniels", "Developer", "Designer"],
+  typeSpeed: 25,
+  backSpeed: 25,
+  loop: true,
+  backDelay: 3500,
+  smartBackspace: true,
+});
